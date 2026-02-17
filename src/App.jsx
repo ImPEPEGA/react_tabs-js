33a18ba84d810e90fa84a9bf5d0a5c76e7b8971f
ImPEPEGA <nicaly15117@gmail.com>
2026-02-17 09:54:17 -0500

    Solution 3

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,12 @@ export const tabs = [
 
 export const App = () => {
   const [activeTabId, setTabSelected] = useState('tab-1');
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
 
   return (
     <div className="section">
       <h1 className="title">
-        Selected tab is {tabs.find(tab => tab.id === activeTabId)?.title}
+        Selected tab is {activeTab.title}
       </h1>
 
       <Tabs
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,24 +1,26 @@
 export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
   // let currentTab = tabs.find(tab => tab.id === activeTabId);
-  if (!tabs.find(tab => tab.id === activeTabId)) {
-    onTabSelected(tabs[0].id);
-  }
+  // if (!tabs.find(tab => tab.id === activeTabId)) {
+  //   onTabSelected(tabs[0].id);
+  // }
+  const activeTab = tabs.find(tab => tab.id === activeTabId) ? activeTabId : tabs[0].id;
 
   return (
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
           {tabs.map(tab => {
-
             return (
               <li
                 key={tab.id}
-                className={activeTabId === tab.id ? 'is-active' : ''}
+                className={activeTab === tab.id ? 'is-active' : ''}
                 data-cy="Tab"
               >
                 <a
                   href={`#${tab.id}`}
-                  onClick={() => activeTabId !== tab.id && onTabSelected(tab.id)}
+                  onClick={() =>
+                    activeTab !== tab.id && onTabSelected(tab.id)
+                  }
                   data-cy="TabLink"
                 >
                   {tab.title}
@@ -30,7 +32,7 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
       </div>
 
       <div className="block" data-cy="TabContent">
-        {tabs.find(tab => tab.id === activeTabId)?.content}
+        {tabs.find(tab => tab.id === activeTab)?.content}
       </div>
     </div>
   );
